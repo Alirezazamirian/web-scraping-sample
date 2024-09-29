32b8d89aaa675b4842e78666e5b3ebd7173368de
Alirezazamirian <alirezazamirian@gmail.com>
2024-09-29 17:01:33 +0330

    edit html.parser to xml

--- cell_service.py
+++ cell_service.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
+import openpyxl
 
 base_url = 'https://rmgsm.ir'
 
 
 def main():
     r = requests.get(base_url + '/sitemap_index.xml')
-    result = []
+    r.encoding = 'utf-8'
+    titles = []
+    brands = []
+    models = []
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, 'xml')
         urls = soup.find_all('loc')
@@ -23,17 +27,49 @@ def main():
             product_urls = product_soup.find_all('loc')
             for new_loc in [loc.text for loc in product_urls]:
                 new_loc_url = requests.get(new_loc)
-                soup = BeautifulSoup(new_loc_url.text, 'html.parser')
+                if '.jpg' in new_loc or '.png' in new_loc:
+                    continue
+                soup = BeautifulSoup(new_loc_url.text, 'xml')
+                print(url)
+                print(new_loc)
                 title = soup.select_one('h1.product_title').get_text(strip=True) if soup.select_one(
                     'h1.product_title') else None
-                result.append(title)
+                brand = None
+                model = None
+                for li in soup.find_all('li'):
+                    title_span = li.find('span', class_='title')
+                    if title_span and 'برند' in title_span.text:
+                        brand = li.text.replace(title_span.text, "").strip()
+                    if title_span and 'مدل' in title_span.text:
+                        model = li.text.replace(title_span.text, "").strip()
+
                 print(title)
+                print(brand)
+                print(model)
+                titles.append(title)
+                brands.append(brand)
+                models.append(model)
     else:
         print(f"Failed to retrieve the sitemap. Status code: {r.status_code}")
 
     with open('./product_data.txt', 'w', encoding='utf-8') as f:
-        for data in result:
-            f.write(data)
+        for i in range(len(titles)):
+            f.write(titles[i])
+            f.write(brands[i])
+            f.write(models[i])
+
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet.title = 'Data'
+    sheet.append(['title', 'brand', 'model'])
+
+    num_rows = min(len(titles), len(brands), len(models))
+    for i in range(num_rows):
+        row_data = [titles[i], brands[i], models[i]]
+        sheet.append(row_data)
+
+    file_path = 'created_data.xlsx'
+    wb.save(file_path)
 
 
 if __name__ == '__main__':
